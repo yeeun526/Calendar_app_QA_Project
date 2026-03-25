11c9148a9b2c8c35ed86ec313d7548131ef26f94
yeeun526 <yeeun4155@naver.com>
2026-03-25 21:59:49 +0900

    csv로 가져와서 sqlite 실행해보기

--- SQL 분석 쿼리/QA_Ddefect_Analysis.sql
+++ SQL 분석 쿼리/QA_Ddefect_Analysis.sql
@@ -5,12 +5,12 @@ FROM test_results -- 1. test_results 라는 엑셀 시트 가져옴
 WHERE Status = 'Bug Found' -- 2. Bug Found 행만 남김
 GROUP BY Module; -- 3. Module 이름별로 끼리끼리 모음
 
--- 2. **중요도별** 결함 밀도 측정
+-- 2. **중요도별** 결함 밀도 측정 <<마지막에 세미콜론 꼭!!>>
 SELECT TC_ID, Title -- 3. where에서 선택된 행의 TC_ID와 title 보여줌
 FROM test_results -- 1. test_result라는 엑셀 시트 가져옴
-WHERE Status = 'Bug Found' AND Priority = 'P1(Urgent)' -- 2. Bug Found에다가 P1(Urgent)인 행만 남김
+WHERE Status = 'Bug Found' AND Priority = 'P1'; -- 2. Bug Found에다가 P1(Urgent)인 행만 남김
 
 -- 3. 전체 **합격률(Pass rate)** 확인
 SELECT 
-(COUNT(CASE WHEN Status = 'Done' THEN 1 END)*100.0/COUNT(*)) -- 1. case when == if
-FROM test_results
+(COUNT(CASE WHEN Status = 'Done' THEN 1 END)*100.0/COUNT(*)) AS Pass_rate -- 1. case when == if
+FROM test_results;
